44c044860f2d2ea16047ffd002cfb53fb33c82a1
Nick LaMuro <nicklamuro@gmail.com>
2020-05-18 14:26:49 -0500

    [GitService::Credentials] Add host_config
    Allows setting a @host_config to GitService::Credentials, and using that
    to fetch auth instead of through the ssh-agent.

--- lib/git_service/credentials.rb
+++ lib/git_service/credentials.rb
@@ -1,7 +1,38 @@
 module GitService
   class Credentials
+    # provide a nested set of hashes, each with a hash that can be passed to
+    # Rugged::Credentials::SshKey.
+    #
+    # Example:
+    #
+    #   GitService::Credentials.host_config = {
+    #     '*' => {
+    #       :username    => 'git',
+    #       :private_key => '~/.ssh/id_rsa'
+    #     },
+    #     'github.com' => {
+    #       :username    => 'git',
+    #       :private_key => '~/.ssh/id_rsa'
+    #     }
+    #   }
+    #
+    def self.host_config=(host_config = {})
+      @host_config = host_config
+    end
+
     def self.from_ssh_agent(username)
       Rugged::Credentials::SshKeyFromAgent.new(:username => username)
+    end
+
+    def self.from_hash_config_for_host(host)
+      return nil unless defined?(@host_config)
+
+      ssh_key_config   = @host_config[host]
+      ssh_key_config ||= @host_config['*'] || @host_config[:*]
+
+      if ssh_key_config
+        Rugged::Credentials::SshKey.new(ssh_key_config)
+      end
     end
   end
 end
