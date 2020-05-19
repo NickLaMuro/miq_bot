273caaff9eefef646b5be509620585085c41aca3
Nick LaMuro <nicklamuro@gmail.com>
2020-05-18 19:46:45 -0500

    [services_initializer.rb] Configure ssh config
    Enables configuring an ssh private key configuration in settings for
    Rugged that will no longer rely on the ssh-agent of the machine to be
    running to work.

--- config/initializers/services_initializer.rb
+++ config/initializers/services_initializer.rb
@@ -2,4 +2,8 @@ unless Rails.env.test?
   BugzillaService.credentials = Settings.bugzilla_credentials
   BugzillaService.product     = Settings.bugzilla.product
   PivotalService.credentials  = Settings.pivotal_credentials
+
+  if Settings.git_service_hostname_ssh_config
+    GitService::Credentials.host_config = Settings.git_service_hostname_ssh_config
+  end
 end
